bf62937810c3223fd44cb64531b4d5af0b0c3381
Marty Sloan <marty@karmatek.io>
2022-12-20 21:25:26 -0500

    Initial commit

---
+++ app.php
@@ -0,0 +1,117 @@
+<?php
+
+/*
+	Plugin Name: PCP Finder
+	Plugin URI:
+	Description: Allows users to search by address for their Precinct Committee Person
+	Version: 0.1
+	Author: Marty Sloan
+	Author URI: https://petersrepublicans.com
+	License: GPLv2
+*/
+
+if (!function_exists('write_log')) {
+
+    function write_log($log) {
+        if (true === WP_DEBUG) {
+            if (is_array($log) || is_object($log)) {
+                error_log(print_r($log, true));
+            } else {
+                error_log($log);
+            }
+        }
+    }
+
+}
+// Add Tailwind CSS styles
+wp_enqueue_style( 'ptrc-tailwindcss', plugin_dir_url( __FILE__ ) . 'dist/output.css' );
+
+// Register plugin
+add_shortcode( 'ptrc_pcp_finder', 'ptrc_pcp_finder' );
+
+// Load Alpine.js component for PCP search
+function ptrc_pcp_finder($atts): bool|string
+{
+    ob_start();
+    include dirname( __FILE__ ) . '/index.html';
+    return ob_get_clean();
+}
+
+// Register REST API endpoint for PCP searches
+add_action('rest_api_init', function () {
+    register_rest_route( 'ptrc/v1', '/pcp-search', [
+        'methods' => 'GET',
+        'callback' => 'ptrc_search_pcp',
+    ]);
+    register_rest_route( 'ptrc/v1', '/pcp-address-details', [
+        'methods' => 'GET',
+        'callback' => 'ptrc_pcp_address_details',
+    ]);
+} );
+
+function ptrc_search_pcp(WP_REST_Request $request): WP_REST_Response
+{
+    $address = $request['address'];
+    $endpoint = 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/suggest' .
+        '?f=json' .
+        "&text=$address" .
+        '&maxSuggestions=10' .
+        '&location={"x":-8925596.771804526,"y":4905995.34432425,"spatialReference":{"wkid":102100,"latestWkid":3857}}' .
+        '&distance=50000';
+    $data = json_decode(wp_remote_get($endpoint)['body'], true);
+    $response = new WP_REST_Response($data['suggestions']);
+    $response->set_status( 201 );
+    return $response;
+}
+
+function ptrc_pcp_address_details(WP_REST_Request $request): WP_REST_Response
+{
+    $location = $request['location'];
+    $magicKey = $request['magicKey'];
+    $endpoint = 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates' .
+        "?SingleLine=$location,USA" .
+        '&f=json' .
+        '&outSR={"wkid":102100,"latestWkid":3857}' .
+        '&outFields=Addr_type,Match_addr,StAddr,City' .
+        "&magicKey=$magicKey" .
+        '&distance=50000&location={"x":-8925596.771804526,"y":4905995.34432425,"spatialReference":{"wkid":102100,"latestWkid":3857}}' .
+        '&maxLocations=6';
+    $data = json_decode(wp_remote_get($endpoint)['body'], true);
+    $location = $data['candidates'][0]['location'] ?? null;
+
+    if(is_null($location)) {
+        return_no_results();
+    }
+
+    $endpoint = 'https://services2.arcgis.com/LgK9DpUhNjdU0HLy/arcgis/rest/services/Elections/FeatureServer/1/query' .
+        '?f=json' .
+        '&where=' .
+        '&returnGeometry=false' .
+        '&spatialRel=esriSpatialRelIntersects' .
+        '&geometry={"x":' . $location['x'] . ',"y":' . $location['y'] . ',"spatialReference":{"wkid":102100,"latestWkid":3857}}' .
+        '&geometryType=esriGeometryPoint' .
+        '&inSR=102100' .
+        '&outFields=*' .
+        '&outSR=102100';
+
+    $data = json_decode(wp_remote_get($endpoint)['body'], true);
+    $precinct = $data['features'][0]['attributes'] ?? [];
+
+    if(empty($precinct) || count(str_split($precinct['district_number'])) !== 2) {
+        return_no_results();
+    }
+
+    $pcpTable = include(dirname( __FILE__ ) . '/committeePersons.php');
+    $precinct['pcp'] = $pcpTable[$precinct['district_number']];
+
+    $response = new WP_REST_Response($precinct);
+    $response->set_status( 201 );
+    return $response;
+}
+
+function return_no_results(): WP_REST_Response
+{
+    $response = new WP_REST_Response([]);
+    $response->set_status( 201 );
+    return $response;
+}
---
+++ committeePersons.php
@@ -0,0 +1,100 @@
+<?php
+
+return [
+    'A1' => [
+        'committeePersons' => [
+            'David Ball',
+            'Lucy Christoforetti'
+        ],
+        'extension' => '1001',
+        'email' => 'a1@petersrepublicans.com'
+    ],
+    'A2' => [
+        'committeePersons' => [
+            'Ariele Battista',
+            'Sam Perlmutter'
+        ],
+        'extension' => '1002',
+        'email' => 'a2@petersrepublicans.com'
+    ],
+    'A3' => [
+        'committeePersons' => [
+            'Allan Bruce',
+            'Jodie Sherman'
+        ],
+        'extension' => '1003',
+        'email' => 'a3@petersrepublicans.com'
+    ],
+    'B1' => [
+        'committeePersons' => [
+            'Debbie Weiss',
+            'Steve Renz'
+        ],
+        'extension' => '1004',
+        'email' => 'b1@petersrepublicans.com'
+    ],
+    'B2' => [
+        'committeePersons' => [
+            'Anton Knaus',
+            'Lawna Blankenship'
+        ],
+        'extension' => '1005',
+        'email' => 'b2@petersrepublicans.com'
+    ],
+    'B3' => [
+        'committeePersons' => [
+            'Randy O\'Connell',
+            'Kathleen Chaudhari'
+        ],
+        'extension' => '1006',
+        'email' => 'b3@petersrepublicans.com'
+    ],
+    'C1' => [
+        'committeePersons' => [
+            'Lori McRoberts',
+            'Anthony Ricci'
+        ],
+        'extension' => '1007',
+        'email' => 'c1@petersrepublicans.com'
+    ],
+    'C2' => [
+        'committeePersons' => [
+            'Nick Gagianas',
+            'Wendy Bowes'
+        ],
+        'extension' => '1008',
+        'email' => 'c2@petersrepublicans.com'
+    ],
+    'C3' => [
+        'committeePersons' => [
+            'Mike Holtcamp',
+            'Leigha Selvoski'
+        ],
+        'extension' => '1009',
+        'email' => 'c3@petersrepublicans.com'
+    ],
+    'D1' => [
+        'committeePersons' => [
+            'Tabitha Cavanagh',
+            'Alex O\'Neal'
+        ],
+        'extension' => '1010',
+        'email' => 'd1@petersrepublicans.com'
+    ],
+    'D2' => [
+        'committeePersons' => [
+            'Chuck Steeber',
+            'Tracy Melograne'
+        ],
+        'extension' => '1011',
+        'email' => 'd2@petersrepublicans.com'
+    ],
+    'D3' => [
+        'committeePersons' => [
+            'Frank Kosir, Jr',
+            'Tammy Wagner'
+        ],
+        'extension' => '1012',
+        'email' => 'd3@petersrepublicans.com'
+    ],
+];
